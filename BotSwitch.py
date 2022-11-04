e277a523d1dfd637caf4e139337ee93d071ef859
Parashar <parasharsangle@gmail.com>
2022-11-04 16:56:33 +0530

    INIT: add BotSwitch file & test cron

---
+++ BotSwitch.py
@@ -0,0 +1,14 @@
+import subprocess
+
+class BotSwitch:
+    def intent(self, command):
+        default = "Incorrect command"
+        return getattr(self, command, lambda: default)()
+
+    def temperature(self):
+        _cmds = ['vcgencmd measure_temp']
+        return subprocess.run(_cmds)
+
+    def list_all_commands(self):
+        return dir(self)
+
---
+++ temperator_monitor_cron.py
@@ -0,0 +1,23 @@
+
+import os
+import telepot
+import time
+import BotSwitch
+import re
+
+# TOKEN = sys.argv[1]  # get token from command-line
+# print(os.environ)
+# aqua4_pi_bot
+botname = 'aqua4_pi_bot' # bot secret will be stored in the env var with this key 
+bot = telepot.Bot(os.environ[botname])
+bot.getMe()
+
+while True:
+    commands = BotSwitch()
+    temp = commands.intent('temperature')
+    _temp = re.findall("\d+\.\d+", temp)[0]
+    # if  float(_temp) > 59:
+    bot.sendMessage('5502113781', f'Temperature alert: System temperature is {temp}, please check')
+        # please check fan
+        # please check the owner of this heavy process
+    time.sleep(3600)
