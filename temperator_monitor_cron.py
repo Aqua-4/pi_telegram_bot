c3f5dc553bfb10daede78f80f63e78ff771732b4
Parashar <parasharsangle@gmail.com>
2022-11-04 17:25:20 +0530

    ENH: try dotenv to load env vars

--- temperator_monitor_cron.py
+++ temperator_monitor_cron.py
@@ -4,11 +4,15 @@ import telepot
 import time
 import BotSwitch
 import re
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # TOKEN = sys.argv[1]  # get token from command-line
 # print(os.environ)
 # aqua4_pi_bot
-botname = 'aqua4_pi_bot' # bot secret will be stored in the env var with this key 
+botname = 'aqua4_pi_bot'  # bot secret will be stored in the env var with this key
 bot = telepot.Bot(os.environ[botname])
 bot.getMe()
 
@@ -17,7 +21,8 @@ while True:
     temp = commands.intent('temperature')
     _temp = re.findall("\d+\.\d+", temp)[0]
     # if  float(_temp) > 59:
-    bot.sendMessage('5502113781', f'Temperature alert: System temperature is {temp}, please check')
-        # please check fan
-        # please check the owner of this heavy process
+    bot.sendMessage(
+        '5502113781', f'Temperature alert: System temperature is {temp}, please check')
+    # please check fan
+    # please check the owner of this heavy process
     time.sleep(3600)
