a31efc6467bbb4df732c6eeac641cadbab906c87
Parashar <parasharsangle@gmail.com>
2022-11-04 18:01:16 +0530

    RENAME: file

--- temperature_monitor_cron.py
+++ temperature_monitor_cron.py
@@ -14,15 +14,17 @@ load_dotenv()
 # aqua4_pi_bot
 botname = 'aqua4_pi_bot'  # bot secret will be stored in the env var with this key
 bot = telepot.Bot(os.environ[botname])
+chat_id = os.environ['chat_id']
 bot.getMe()
 
 while True:
     commands = BotSwitch()
     temp = commands.intent('temperature')
     _temp = re.findall("\d+\.\d+", temp)[0]
+    print(_temp, temp)
     # if  float(_temp) > 59:
     bot.sendMessage(
-        '5502113781', f'Temperature alert: System temperature is {temp}, please check')
+        chat_id, f'Temperature alert: System temperature is {temp}, please check')
     # please check fan
     # please check the owner of this heavy process
     time.sleep(3600)
