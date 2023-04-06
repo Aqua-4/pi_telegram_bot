89646bbb8d17e6b6357d1f0f774f16eb47d4e337
Aqua-4 <parasharsangle@gmail.com>
2023-04-06 17:13:03 +0530

    ENH: refresh script to run on loop

--- refresh_historical_data.py
+++ refresh_historical_data.py
@@ -6,27 +6,24 @@
     11 9 * * 1-5 cd /home/pi/pi-telegram-bot && nohup python3 stock_market_monitor.py > stock_market_monitor_log.txt &
 """
 
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from fyers_utils import FyersUtils
+import time
+import telepot
+from neodb_utils import NeoDB
+from mongodb_utils import MongoDB
 import sys
 import os
- 
+
 # getting the name of the directory
 # where the this file is present.
 parent = os.path.dirname(os.path.realpath('./'))
- 
+
 # adding the parent directory to
 # the sys.path.
-sys.path.append(os.path.join(parent,'fundamental_omniwatcher'))
+sys.path.append(os.path.join(parent, 'fundamental_omniwatcher'))
 
-from mongodb_utils import MongoDB
-from neodb_utils import NeoDB
-
-
-import os
-import telepot
-import time
-from fyers_utils import FyersUtils
-from dotenv import load_dotenv
-from datetime import datetime,timedelta
 
 load_dotenv()
 load_dotenv("db.env")
@@ -82,12 +79,15 @@ def get_fyers_session():
     else:
         pass
 
+
 # neo4j & mongo init intance here
-neo_instance = NeoDB(username='neo4j', password='Gramener@123', host=os.environ['host'])
-mongo_instance = MongoDB(username='mongo_root', password='Gramenr123', host=os.environ['host'])
+neo_instance = NeoDB(
+    username='neo4j', password='Gramener@123', host=os.environ['host'])
+mongo_instance = MongoDB(username='mongo_root',
+                         password='Gramenr123', host=os.environ['host'])
 
 
-fu = FyersUtils(bot_send_message,mongo_instance)
+fu = FyersUtils(bot_send_message, mongo_instance)
 try:
     get_fyers_session()
 except Exception as err:
@@ -99,8 +99,7 @@ fyers = fu.fyers
 # normal fyers login upto this point
 
 
-
-def download_data_chunk(ticker_name, chunk_in_days = 99):
+def download_data_chunk(ticker_name, chunk_in_days=99):
     """
     Download data from 2019
 
@@ -117,51 +116,56 @@ def download_data_chunk(ticker_name, chunk_in_days = 99):
 
     """
     # TODO: if the stock exists on fyers check in mongo
-    _mongo_collection =  mongo_instance.use_collection(ticker_name)
+    _mongo_collection = mongo_instance.use_collection(ticker_name)
     # TODO: if stock exists download the data for x years & dump into mongo DB
-    min_date, max_date =  mongo_instance.get_minmax_dates(_mongo_collection)
+    min_date, max_date = mongo_instance.get_minmax_dates(_mongo_collection)
 
     ymd_format = "%Y-%m-%d"
     today = datetime.now()
-        
-    start_date = datetime.strptime( "2019-01-01", ymd_format)
- 
+
+    start_date = datetime.strptime("2019-01-01", ymd_format)
+
     # TODO: if stock exists in mongo check the min & max - use this delta to cut down cost. Need to do same for min_date
     if max_date and start_date < max_date:
         start_date = max_date
     to_date = (start_date + timedelta(days=99))
-    
+
     if to_date > today:
         to_date = (today - timedelta(minutes=1))
-    
-    
+
     while to_date < today:
         _fyers_symbol_name = f"NSE:{ticker_name}-EQ"
-        print( start_date.strftime(ymd_format) , 'to' , to_date.strftime(ymd_format) )
-        fu.dump_historical_data_equity(ticker_name, symbol_name = _fyers_symbol_name, from_date = start_date.strftime(ymd_format) , to_date = to_date.strftime(ymd_format))
+        print(start_date.strftime(ymd_format),
+              'to', to_date.strftime(ymd_format))
+        fu.dump_historical_data_equity(ticker_name, symbol_name=_fyers_symbol_name, from_date=start_date.strftime(
+            ymd_format), to_date=to_date.strftime(ymd_format))
 
         start_date = (start_date + timedelta(days=99))
         to_date = (start_date + timedelta(days=99))
         if to_date > today and start_date < today:
-            print( start_date.strftime(ymd_format) , 'to' , today.strftime(ymd_format) )
-            fu.dump_historical_data_equity(ticker_name, symbol_name = _fyers_symbol_name, from_date = start_date.strftime(ymd_format) , to_date = today.strftime(ymd_format))
-    
-        
+            print(start_date.strftime(ymd_format),
+                  'to', today.strftime(ymd_format))
+            fu.dump_historical_data_equity(ticker_name, symbol_name=_fyers_symbol_name, from_date=start_date.strftime(
+                ymd_format), to_date=today.strftime(ymd_format))
+
+
 # df = fu.download_historical_data('NSE:BIOCON-EQ',granularity_in_mins=1, from_date="2022-9-1", to_date="2022-10-1")
-# df.to_csv('biocon-intraday_prev2.csv')    
+# df.to_csv('biocon-intraday_prev2.csv')
 
 
 # d_df = df[ (df.index > '2022-09-1') & (df.index < '2022-09-3') ]
- 
-# TODO: get all ticker names from neo4j 
+
+# TODO: get all ticker names from neo4j
 # all_stocks = neo_instance.get_all_nodes()
 # # for each tickername check if the equity stock exists on fyers
 # for stock_meta in all_stocks:
 #     ticker_name = stock_meta.get("ticker_name")
 
-# get all ticker names from neo4j 
-all_stocks = neo_instance.get_all_nodes()
-# for each tickername check if the equity stock exists on fyers
-for stock_meta in all_stocks:
-    ticker_name = stock_meta.get("ticker_name")    
-download_data_chunk('DLINKINDIA')
+now = datetime.now()
+today900am = datetime.now().replace(hour=9, minute=0, second=0, microsecond=0)
+today330pm = datetime.now().replace(hour=15, minute=30, second=0, microsecond=0)
+
+while datetime.now() > today900am and datetime.now() < today330pm:
+    download_data_chunk('DLINKINDIA')
+    # wait for an hour before downloading the new data
+    time.sleep(3600)
